1b350e386fd9e65761b67858d76b5092240b5012
samuel Gomez <samgomhay@alu.edu.gva.es>
2024-10-02 08:49:36 +0200

    ejercicio17.sh

--- bash/ejercicio16.sh
+++ bash/ejercicio16.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
-suma_f=0
-for (( i=1; i<=1000; i++ )); do
-  suma_f=$((suma_f + i))
+
+suma=0
+for i in {1..1000}; do
+  ((suma+=i))
 done
+  echo "la suma con for es: $suma"
 
+suma=0
+i=1
+ while [ $i -le 1000 ]; do
+  ((suma+=i))
+  ((i++))
+ done
+  echo "la suma con while es: $suma"
 
+suma=0
+i=1
+ until [ $i -gt 1000 ]; do
+  ((suma+=i))
+  ((i++))
+ done
+echo "la suma con until es : $suma"
