aca7b38bbb388a16f95e033adb475fb93f59d6da
samuel Gomez <samgomhay@alu.edu.gva.es>
2024-10-03 12:01:58 +0200

    ejercicio26.sh

--- bash/ejercicio25.sh
+++ bash/ejercicio25.sh
@@ -2,4 +2,4 @@
 
 
 echo "ultimos usuarios que iniciaron sesión con sus direcciones IP:"
- last | egrep -o '^[a-zA-Z0-9_.+-]+ + [a-zA-Z0-p_.+-]+ +[0-0]+\.[0-9]+'| awk '{print "usuario: " $1 " , 192.168.122.1: " $3}'
+ last | egrep -o '^[a-zA-Z0-9_.+-]+ + [a-zA-Z0-p_.+-]+ +[0-0]+\.[0-9]+'| awk '{print "usuario: " $1 " , 192.168.0.1: " $3}'
--- bash/ejercicio26.sh
+++ bash/ejercicio26.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+output_file="archivos_peligrosos.txt"
+
+find / -type f -perm -002 -exec realpath {} \; > "$output_file"
+
+echo "La lista de archivos peligrosos ha sido guardada en $output_file"
+
 
