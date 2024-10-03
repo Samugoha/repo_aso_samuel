90adbeabc8d6824513a0b8cd247ae3b83917b3f2
samuel Gomez <samgomhay@alu.edu.gva.es>
2024-10-03 11:55:38 +0200

    ejercicio25.sh

---
+++ bash/ejercicio25.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+echo "ultimos usuarios que iniciaron sesión con sus direcciones IP:"
+ last | egrep -o '^[a-zA-Z0-9_.+-]+ + [a-zA-Z0-p_.+-]+ +[0-0]+\.[0-9]+'| awk '{print "usuario: " $1 " , 192.168.122.1: " $3}'
