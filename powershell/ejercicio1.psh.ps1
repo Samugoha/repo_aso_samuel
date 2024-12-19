11255d9389ab875b7461936500947bf6d8e7eef2
samugoha <samgomhay@alu.edu.gva.es>
2024-12-19 12:38:46 +0100

    Prueba de Powershell

---
+++ powershell/ejercicio1.psh.ps1
@@ -0,0 +1,4 @@
+﻿ Write-Host "hola $env:USERNAME"
+ write-host "tu directorio de trabajo es $PWD"
+ write-host "perteneces al dominio $env:USERDOMAIN"
+ write-host "tu equipo se llama $env:COMPUTERNAME"
