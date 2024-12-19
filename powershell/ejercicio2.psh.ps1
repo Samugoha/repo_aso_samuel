f338ebd56a0201271436e7e38375c0f5da5b3eb1
samugoha <samgomhay@alu.edu.gva.es>
2024-12-19 13:02:06 +0100

    ejercicio2.psh

---
+++ powershell/ejercicio2.psh.ps1
@@ -0,0 +1,13 @@
+﻿[int] $numero = Read-Host "dime un número"
+[int] $numero2 = Read-Host "dime otro número"
+$suma = $numero + $numero2
+$resta = $numero - $numero2
+$multiplicación = $numero * $numero2
+$division = $numero / $numero2
+$resto = $numero % $numero2
+
+Write-Host "la suma es = $numero + $numero2 = $suma "
+Write-Host " la resta es $numero - $numero2 = $resta"
+Write-Host " la multiplicación es $numero * $numero2 = $multiplicación"
+Write-Host " la división es $numero / $numero2 = $division"
+Write-Host " el resto es $numero % $numero2 = $resto"
