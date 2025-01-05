93a066a91def266fe513025c4a5891583acaa13f
Samugoha <samgomhay@alu.edu.gva.es>
2025-01-05 11:46:47 +0100

    powershell

---
+++ powershell/ej 2.10.ps1
@@ -0,0 +1,29 @@
+﻿Write-Host "dime tu puntuación: "
+$puntuacion = [double](Read-Host)
+
+if ($puntuacion -eq 0.0)
+ {
+    $nivel = "Inaceptable"
+    $beneficio = 2400 * $puntuacion
+} 
+elseif ($puntuacion -eq 0.4) 
+{
+    $nivel = "Aceptable"
+    $beneficio = 2400 * $puntuacion
+}
+ elseif ($puntuacion -ge 0.6
+ ) {
+    $nivel = "Meritorio"
+    $beneficio = 2400 * $puntuacion
+}
+ else {
+    $nivel = "Puntuación no válida"
+    $beneficio = 0
+}
+
+if ($nivel -ne "Puntuación no válida") {
+    Write-Host "Tu nivel de rendimiento es: $nivel."
+    Write-Host "La cantidad de dinero que recibirás es: $beneficio €."
+} else {
+    Write-Host "la puntuación es no válida."
+}
