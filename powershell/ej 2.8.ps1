2500777183ea99f647940df1b132870b9fb300ec
Samugoha <samgomhay@alu.edu.gva.es>
2025-01-05 11:45:32 +0100

    powershell

---
+++ powershell/ej 2.8.ps1
@@ -0,0 +1,10 @@
+﻿$nombre = Read-Host "Introduce tu nombre"
+$sexo = Read-Host "Introduce tu sexo (mujer/hombre)"
+$primerLetra = $nombre.Substring(0, 1).ToUpper()
+if ($sexo.ToLower() -eq "mujer" -and $primerLetra -lt "M") {
+    Write-Host "Te corresponde el grupo A."
+} elseif ($sexo.ToLower() -eq "hombre" -and $primerLetra -gt "N") {
+    Write-Host "Te corresponde el grupo A."
+} else {
+    Write-Host "Te corresponde el grupo B."
+}
