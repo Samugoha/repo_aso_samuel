ca91ac8f99e026c170e11a689a875f0eea8cd9d4
samugoha <samgomhay@alu.edu.gva.es>
2025-01-13 12:33:22 +0100

    ejercicio_3_practica_4

---
+++ powershell/4.3.ps1
@@ -0,0 +1,8 @@
+﻿Write-Host "Ingrese un número entero positivo:"
+$num = Read-Host
+if ([int]$num -ge 0) {
+ $cuentaAtras = ($num..0 -join ", ")
+    Write-Host "Cuenta atrás: $cuentaAtras"
+} else {
+    Write-Host "Por favor, ingrese un número entero positivo."
+}
