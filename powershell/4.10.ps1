c7b3b71ffa5523e80bd5548724dce04357cacfc4
samugoha <samgomhay@alu.edu.gva.es>
2025-01-15 09:37:19 +0100

    practica4

---
+++ powershell/4.10.ps1
@@ -0,0 +1,36 @@
+﻿do {
+    Write-Host "Menú:"
+    Write-Host "a) Crear carpeta"
+    Write-Host "b) Crear un fichero nuevo"
+    Write-Host "c) Cambiar el nombre de un fichero o carpeta"
+    Write-Host "d) Borrar un archivo o carpeta"
+    Write-Host "e) Ver si existe un fichero o carpeta"
+    Write-Host "f) Mostrar el contenido de un directorio"
+    Write-Host "g) Mostrar la fecha y hora actuales"
+    Write-Host "x) Salir"
+    $opcion = Read-Host "Seleccione una opción"
+    if ($opcion -eq "a") {
+        New-Item -ItemType Directory -Path (Read-Host "Nombre de la carpeta")
+    } elseif ($opcion -eq "b") {
+        New-Item -ItemType File -Path (Read-Host "Nombre del fichero")
+    } elseif ($opcion -eq "c") {
+        Rename-Item -Path (Read-Host "Nombre actual") -NewName (Read-Host "Nuevo nombre")
+    } elseif ($opcion -eq "d") {
+        Remove-Item -Path (Read-Host "Nombre del fichero o carpeta") -Recurse -Force
+    } elseif ($opcion -eq "e") {
+        if (Test-Path (Read-Host "Nombre del fichero o carpeta")) {
+            Write-Host "Existe."
+        } else {
+            Write-Host "No existe."
+        }
+    } elseif ($opcion -eq "f") {
+        Get-ChildItem -Path (Read-Host "Nombre del directorio")
+    } elseif ($opcion -eq "g") {
+        Get-Date
+    } elseif ($opcion -eq "x") {
+        Write-Host "Saliendo"
+    } else {
+        Write-Host "no válido."
+    }
+    Pause
+} while ($opcion -ne "x")
---
+++ powershell/5.1.ps1
@@ -0,0 +1,47 @@
+﻿do {
+    Clear-Host
+    Write-Host "****** CALCULADORA ********"
+    Write-Host "1) Sumar"
+    Write-Host "2) Restar"
+    Write-Host "3) Multiplicar"
+    Write-Host "4) Dividir"
+    Write-Host "5) Salir"
+    $opcion = Read-Host "¿Qué desea hacer? Elige una opción (1-5)"
+
+    if ($opcion -in 1, 2, 3, 4) {
+        $num1 = [double](Read-Host "Introduce el primer número")
+        $num2 = [double](Read-Host "Introduce el segundo número")
+        switch ($opcion) {
+            1 { $resultado = Sumar -a $num1 -b $num2 }
+            2 { $resultado = Restar -a $num1 -b $num2 }
+            3 { $resultado = Multiplicar -a $num1 -b $num2 }
+            4 { $resultado = Dividir -a $num1 -b $num2 }
+        }
+        Write-Host "Resultado: $resultado"
+    } elseif ($opcion -eq 5) {
+        Write-Host "Saliendo"
+    } else {
+        Write-Host "Opción no válida, intentalo nuevamente."
+    }
+    Pause
+} while ($opcion -ne 5)
+function Sumar {
+    param ($a, $b)
+    return $a + $b
+}
+function Restar {
+    param ($a, $b)
+    return $a - $b
+}
+function Multiplicar {
+    param ($a, $b)
+    return $a * $b
+}
+function Dividir {
+    param ($a, $b)
+    if ($b -ne 0) {
+        return $a / $b
+    } else {
+        return "No se puede dividir entre cero."
+    }
+}
