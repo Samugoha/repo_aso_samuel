e057822475e0332fd10834af15cac631cdfee1fe
samugoha <samgomhay@alu.edu.gva.es>
2025-01-16 11:49:43 +0100

    practica 5

---
+++ powershell/4.11.ps1
@@ -0,0 +1,4 @@
+﻿$Archivo = "C:\Users\Administrador\Downloads\usuarios.csv"
+Import-Csv -Path $Archivo | ForEach-Object {
+    Write-Output "Nombre: $($_.nombre), Apellidos: $($_.apellidos), Grupo: $($_.grupo)"
+}
