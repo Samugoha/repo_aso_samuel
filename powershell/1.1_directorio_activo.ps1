d6712097825f2e36f27c0231f755bc2a78550dc4
samugoha <samgomhay@alu.edu.gva.es>
2025-01-22 09:12:59 +0100

    powershell

---
+++ powershell/1.1_directorio_activo.ps1
@@ -0,0 +1,35 @@
+﻿$departamentos = "C:\Users\Administrador\Downloads\departamentos.csv"
+$empleados = "C:\Users\Administrador\Downloads\empleados.csv"
+$defaultPassword = "aso2025."
+$departamentosdata = Import-Csv -Path $departamentos -Delimiter ";"
+New-ADOrganizationalUnit -name "Empresa" -Path "DC=EMPRESA,DC=LOCAL"
+
+foreach ($departamento in $departamentosdata) {
+$nombredept = $departamento.departamento
+$descripcion = $departamento.descripcion
+
+$ouPath = "OU=Empresa,DC=EMPRESA,DC=LOCAL"
+Write-Host $ouPath
+New-ADOrganizationalUnit -name $nombredept -Description $descripcion -Path $ouPath -ErrorAction SilentlyContinue
+}
+
+$empleadosdata = Import-Csv -Path $empleados -Delimiter ";"
+
+foreach ($empleado in $empleadosdata) {
+$departamento = $empleado.Departamento
+$nombre = $empleado.Nombre
+$apellido = $empleado.Apellido
+
+$login = ($nombre + "." + $apellido).ToLower()
+$ouPath = "OU=$departamento,OU=Empresa,DC=EMPRESA,DC=LOCAL"
+
+New-ADUser -SamAccountName $login `
+    -UserPrincipalName "$login@EMPRESA.LOCAL" `
+    -NAME "$nombre $apellido" `
+    -GivenName $nombre `
+    -surName $apellido `
+    -path $ouPath `
+    -AccountPassword (ConvertTo-SecureString $defaultPassword -AsPlainText -Force) `
+    -Enabled $true `
+    -ChangePasswordAtLogon $true
+}
