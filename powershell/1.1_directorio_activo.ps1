2f0b67db30a7758aaa41e46c2b677aeba78a1805
samugoha <samgomhay@alu.edu.gva.es>
2025-01-22 08:16:03 +0100

    powershell

---
+++ powershell/1.1_directorio_activo.ps1
@@ -0,0 +1,16 @@
+﻿$departamentos = "C:\Users\Administrador\Downloads\departamentos.csv"
+$empleados = "C:\Users\Administrador\Downloads\empleados.csv"
+$defaultPassword = "aso2025."
+$departamentosData = Import-Csv -Path $departamentos
+$empleadosData = Import-Csv -Path $empleados
+$empleadosData | ForEach-Object {
+    $login = "$($_.Nombre.ToLower()).$($_.Apellido.ToLower())"
+    if ($departamentosData.Departamento -contains $_.Departamento) {
+        New-LocalUser -Name $login -Password (ConvertTo-SecureString $defaultPassword -AsPlainText -Force) `
+                      -PasswordNeverExpires:$false -UserMayNotChangePassword:$false `
+                      -FullName "$($_.Nombre) $($_.Apellido)"
+        Write-Host "Usuario '$login' creado."
+    } else {
+        Write-Host "El departamento '$($_.Departamento)' no existe. Usuario no creado."
+    }
+}
